f44f2e5c8ce39c1378cb15d3c5ff721e428cbc75
Prateek Diwedi <diwedi1112@gmail.com>
2020-05-19 23:57:46 +0530

    updated landing page

---
+++ src/components/HomePageText.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function HomePageText() {
+  return (
+    <div className="name">
+    <h1>
+    Hello, I'm Prateek Diwedi.
+      <p>
+         I'm a Full Stack Developer
+      </p>
+    </h1>
+  
+    </div>
+  )
+}
+
+export default HomePageText;
