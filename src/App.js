d7c82e4603eee72f15718072de571e21043865e6
Prateek Diwedi <diwedi1112@gmail.com>
2020-05-20 15:11:19 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-     
+     <h1>
+    Hello, I'm Prateek Diwedi.
+      <p>
+         I'm a Full Stack Developer
+      </p>
+    </h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import HomePageText from './components/HomePageText';
+import Navbar from './components/NavBar'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Navbar />
     <App />
-    <HomePageText/>
   </React.StrictMode>,
   document.getElementById('root')
 );
