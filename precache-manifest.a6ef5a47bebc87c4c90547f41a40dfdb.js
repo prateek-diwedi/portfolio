d256247e06dc2db5a9a9fc69f1eae8fe03364c58
Prateek Diwedi <diwedi1112@gmail.com>
2020-05-22 22:37:46 +0530

    Updates

---
+++ precache-manifest.a6ef5a47bebc87c4c90547f41a40dfdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160dfef43a7e62fbfd97a4b8510f27bc",
+    "url": "/github.io/portfolio/index.html"
+  },
+  {
+    "revision": "95e99c62e213d4e91c1c",
+    "url": "/github.io/portfolio/static/css/2.0c8fc136.chunk.css"
+  },
+  {
+    "revision": "fe5ab219e506fa823774",
+    "url": "/github.io/portfolio/static/css/main.c188f064.chunk.css"
+  },
+  {
+    "revision": "95e99c62e213d4e91c1c",
+    "url": "/github.io/portfolio/static/js/2.e6a408f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/portfolio/static/js/2.e6a408f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe5ab219e506fa823774",
+    "url": "/github.io/portfolio/static/js/main.9553c6ce.chunk.js"
+  },
+  {
+    "revision": "db9d92a67140571f15b8",
+    "url": "/github.io/portfolio/static/js/runtime-main.481fd647.js"
+  }
+]);
