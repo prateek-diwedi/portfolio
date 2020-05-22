d1d7a3fa283ef7261bc350583349edb900dfa80d
Prateek Diwedi <diwedi1112@gmail.com>
2020-05-22 22:45:25 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div className="App" id='homeAnchor'>
-     <h1>
-    Hello, I'm Prateek Diwedi.
+      <h1>
+        Hello, I'm Prateek Diwedi.
       <p>
-         I'm a Full Stack Developer
+          I'm a Full Stack Developer
       </p>
-    </h1>
+      </h1>
+      <div class="wrapper">
+        <a id='landingPageButton' href="#aboutMeAnchor"><span>View My Work!</span></a>
+      </div>
     </div>
   );
 }
